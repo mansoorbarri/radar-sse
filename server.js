76ec5e7fb7118aaafdc0d48ff7edf75a21a1ea6f
Mansoor Barri <mansoor.eb.ak@gmail.com>
2025-12-27 01:20:53 +0000

    fix PrismaClient not found issue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require("./generated/client");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
