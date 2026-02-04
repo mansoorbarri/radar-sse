653b9af56fc2ceb85a14adbe12ef26358ab09dc9
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-02-04 21:39:23 +0000

    add CLAUDE.md + add linting setup

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-undef": "error",
+      "no-console": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double", { avoidEscape: true }],
+      "eqeqeq": ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+    },
+  },
+  {
+    ignores: ["node_modules/"],
+  },
+];
